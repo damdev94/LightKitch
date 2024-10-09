e945fe31f26f6ad475b510c07f3c4df2f9cc0afb
damien duport percier <damienviel69@gmail.com>
2024-10-09 15:19:39 +0200

    top part

--- src/pages/CommentCaMarche.js
+++ src/pages/CommentCaMarche.js
@@ -1,10 +1,41 @@
 import React from 'react'
 import { motion } from 'framer-motion';
+import GlobalFooter from '../components/globalFooter/GlobalFooter';
+import '../css/pages/commentCaMarche.scss'
+import { Link } from 'react-router-dom';
 
 function CommentCaMarche({pageVariants}) {
   return (
     <motion.div initial="initial" animate="animate" exit="exit" variants={pageVariants} className='commentCaMarche-container'>
-      <h1>Comment ca marche</h1>
+
+      <div className='commentCaMarche-presentation'>
+
+        <div className='presentation-left'>
+
+          <div className='left-content'>
+            <h2>Comment <strong>ça marche</strong></h2>
+            <p>Découvrez comment notre solution clé en main peut transformer votre activité.</p>
+            <Link to='/rejoignez-nous'>Prendre RDV</Link>
+          </div>
+
+        </div>
+
+        <div className='presentation-right'>
+          <div className='right-content'>
+
+          </div>
+        </div>
+
+      </div>
+
+      <div className='commentCaMarche-promotion'>
+        <div className='promotion-content'>
+          <p>Propulsez votre restaurant vers de nouveaux sommets grâce à Light Kitch, votre partenaire expert en restaurants virtuels. Nous nous occupons de tout, de la création de marques virtuelles sur mesure à l’optimisation de vos menus et de vos campagnes marketing, afin de maximiser votre visibilité et vos revenus sur les plateformes de livraison.</p>
+        </div>
+      </div>
+
+      <GlobalFooter />
+
     </motion.div>
   )
 }
